3dc7ca51b42dd61d39c7be01d830006a6620039a
Matthew Bendall <bendall@gwu.edu>
2015-05-08 10:22:01 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='telescope',
+      version='0.1',
+      description='Single locus resolution of Transposable ELEment expression using next-generation sequencing.',
+      author='Matthew L. Bendall',
+      author_email='bendall@gwu.edu',
+      packages=['telescope'],
+      zip_safe=False)
