9d370b4102567857ac7e3ba228d384fb58c3406d
Matt <matt.greenig@gmail.com>
2021-09-30 16:04:49 -0400

    added progress output

--- telescope/utils/cellsplit.py
+++ telescope/utils/cellsplit.py
@@ -42,7 +42,7 @@ class CellSplit:
                     barcode_alignments[cbc].append(read.to_string())
 
                     if sum(reads_per_barcode.values()) % 2.5e6 == 0 and not self.opts.quiet:
-                        lg.info(f'{sum(reads_per_barcode.values()) / 1e6:.2f}M alignments processed')
+                        lg.info(f'...{sum(reads_per_barcode.values()) / 1e6:.1f}M alignments processed')
 
         # write cell-level bam files
         for cbc, file in file_handles.items():
